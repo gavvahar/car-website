4557fe35ecf70e86fe2838ec2cebb7eb354aed10
gavvahar <nihargavva@gmail.com>
2022-09-10 01:28:12 -0400

    testing page

---
+++ views/places/test.jsx
@@ -0,0 +1,17 @@
+const React = require('react')
+const Def = require('../default')
+
+function test(){
+    return(
+        <Def>
+            <head>
+                <title>Test Page</title>
+            </head>
+            <main>
+               <p>This is to test shit</p>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = test
