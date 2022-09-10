50f46a88554b95e648a704db49fe9f7f47fd8a78
gavvahar <nihargavva@gmail.com>
2022-09-10 19:57:35 -0400

    adding logo to the tab

--- views/default.jsx
+++ views/default.jsx
@@ -4,11 +4,11 @@ function Def(html){
         <html>
             <head>
                 {/* <title>Home Page</title> */}
+                <link rel="icon" href="../images/racers-heaven-logo.jpg" />
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"/>
 				<link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
-                <h1>Racer's Heaven</h1>
                 <nav>
                     <ul>
                         <li>
@@ -18,7 +18,7 @@ function Def(html){
                             <a href="/places">Places</a>
                         </li>
                         <li>
-                            <a href="/places/new">Add Place</a>
+                            <a href="/places/new">Add Car</a>
                         </li>
                         <li>
                             <a href="/places/about">About</a>
