745cb2279b1a5fc2ce7056b9f4a0e84959997199
gavvahar <nihargavva@gmail.com>
2022-10-03 21:18:15 -0400

    testing page

--- views/places/test.jsx
+++ views/places/test.jsx
@@ -1,17 +1,72 @@
+// const React = require('react')
+// const Def = require('../default')
+
+// function test(){
+//     return(
+//         <Def>
+//             <head>
+//                 <title>Test Page</title>
+//             </head>
+//             <main>
+//                <p>This is to test shit!!!!!!!</p>
+//             </main>
+//         </Def>
+//     )
+// }
+
+// module.exports = test
+
 const React = require('react')
 const Def = require('../default')
 
-function test(){
-    return(
+function new_form (data) {
+    let message = ''
+    if(data.message)
+    {
+        message = (
+            <h4 className='alert alert-danger'>{data.message}</h4>
+        )
+    }
+    return (
         <Def>
-            <head>
-                <title>Test Page</title>
-            </head>
-            <main>
-               <p>This is to test shit!!!!!!!</p>
-            </main>
+          <main>
+            <h1>Add a New Car</h1>
+            {message}
+            <form method="POST" action={`/places`}>
+                <div className="form-group">
+                    <label htmlFor="make">Car Make</label>
+                    <input className="form-control" id="make" name="make" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="pic">Place Picture</label>
+                    <input className="form-control" id="pic" name="pic" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="city">City</label>
+                    <input className="form-control" id="city" name="city" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="state">State</label>
+                    <input className="form-control" id="state" name="state" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="cuisines">Cuisines</label>
+                    <input className="form-control" id="cuisines" name="cuisines" required />
+                </div>
+                <div className="form-group col-sm-4">
+                    <label htmlFor="founded">Founded Year</label>
+                    <input 
+                        type="number"
+                        className="form-control"
+                        id="founded"
+                        name="founded" 
+                        value={new Date().getFullYear()} />
+                    </div>
+                <input className="btn btn-primary" type="submit" value="Add Car" />
+            </form>
+          </main>
         </Def>
     )
 }
 
-module.exports = test
+module.exports = new_form
