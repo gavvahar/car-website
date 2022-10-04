c412383238e637060e63e370d93c04a057d220bb
gavvahar <nihargavva@gmail.com>
2022-10-03 21:18:58 -0400

    tryuing to add cars to the list

---
+++ models/cars.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+let carSchema = new mongoose.Schema({
+  year: {
+    type: Number,
+    min: [1673, "Surely not that old?!"],
+    max: [new Date().getFullYear(), "This is the future!"],
+    required: true,
+  },
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  pic: { type: String, default: "../public/images/pic.jpg" },
+  trim: { type: String, default: "N/A" },
+});
+
+module.exports = mongoose.model("Car", carSchema);
---
+++ seeders/seed-cars.js
@@ -0,0 +1,28 @@
+const db = require("../models");
+
+db.Car.create([
+  {
+    founded: 2015,
+    make: "Lexus",
+    model: "IS250",
+    state: "WA",
+    trim: "F-Sport",
+    // pic: "/images/large.jpg",
+  },
+  //   {
+  //     name: "Coding Cat Cafe",
+  //     city: "Phoenix",
+  //     state: "AZ",
+  //     cuisines: "Coffee, Bakery",
+  //     pic: "/images/the-interior.jpg",
+  //     founded: 2020,
+  //   },
+])
+  .then(() => {
+    console.log("Success!");
+    process.exit();
+  })
+  .catch((err) => {
+    console.log("Failure!", err);
+    process.exit();
+  });
