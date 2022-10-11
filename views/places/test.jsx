676df681d4b30d070ffb0aa6029e8363fd28dfbc
gavvahar <nihargavva@gmail.com>
2022-10-11 12:30:26 -0400

    restoring test page

--- views/places/test.jsx
+++ views/places/test.jsx
@@ -1,53 +1,17 @@
-// const React = require('react')
-// const Def = require('../default')
-
-// function test(){
-//     return(
-//         <Def>
-//             <head>
-//                 <title>Test Page</title>
-//             </head>
-//             <main>
-//                <p>This is to test shit!!!!!!!</p>
-//             </main>
-//         </Def>
-//     )
-// }
-
-// module.exports = test
-
 const React = require('react')
 const Def = require('../default')
 
-function test (data) {
-  let carsFormatted = data.cars.map((car) => {
-    return (
-      <div key="1" className='col-sm-6'>
-        <h2 className='text-center'>
-            <a href={`/places/${car.id}`}>
-              {car.make}
-            </a>
-          </h2>
-        <p className='text-center'>
-          {car.model}
-        </p>
-        <img src={car.pic} alt={car.model} className='center' width="100%" height="50%"/>
-        {/* <p className='text-center'>
-          Located in {place.city}, {place.state}
-        </p> */}
-      </div>
+function test(){
+    return(
+        <Def>
+            <head>
+                <title>Test Page</title>
+            </head>
+            <main>
+               <p>This is to test shit!!!!!!!</p>
+            </main>
+        </Def>
     )
-  })
-  return (
-    <Def>
-        <main>
-            <h1>Welcome to Racer's Heaven</h1>
-            <div className='row'>
-              {carsFormatted}
-            </div>
-        </main>
-    </Def>
-)
 }
 
 module.exports = test
