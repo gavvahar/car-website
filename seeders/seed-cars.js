3da3e433a543518172c4c3fd83c80270334d919a
gavvahar <nihargavva@gmail.com>
2022-10-11 12:30:00 -0400

    changed pic

--- seeders/seed-cars.js
+++ seeders/seed-cars.js
@@ -6,7 +6,7 @@ db.Car.create([
     make: "Lexus",
     model: "IS250",
     trim: "F-Sport",
-    pic: "/images/pic.jpg",
+    pic: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/2014_Lexus_IS250_F_Sport_Package_LA.jpg/280px-2014_Lexus_IS250_F_Sport_Package_LA.jpg",
   },
 ])
   .then(() => {
